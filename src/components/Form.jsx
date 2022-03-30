0eb7b34ef55bb54e8a3fa3e96a6f80bd4d2b79cb
AlexisLeonardoFeria <alexisferia700@gmail.com>
2022-03-29 20:57:42 -0500

    correccion de una letra

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -185,7 +185,7 @@ function Form() {
             )
           }
         </form>
-        <h2>Integrantes: Alexis Feria, Dayanara Tamayo, Melissa Quiñones</h2>
+        <h2>Integrantes: Alexis Feria, Dayanara Tamayo, Melissa Quiñonez</h2>
       </div>
 
       <div className='container--sales'>
